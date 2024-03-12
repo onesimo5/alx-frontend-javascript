db1652884f612860b44ee61c00f75517ef137290
onesimo5 <onesimojora57@gmail.com>
2024-03-12 15:58:31 +0000

    task10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,12 @@
 export default function updateUniqueItems(map) {
-  const list = map;
-
-  if (list instanceof Map) {
-    for (const [key, value] of list) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
       if (value === 1) {
-        list.set(key, 100);
+        map.set(key, 100);
       }
     }
   } else {
     throw new Error('Cannot process');
   }
-  return list;
+  return map;
 }
